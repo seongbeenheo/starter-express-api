881daa34cf3dabdadec392997b7cf0206bc2b038
seongbeenheo <rayheo0225@naver.com>
2024-04-04 19:54:07 +0900

    add post route

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+
+app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.post("/send", (req, res) => {
+    res.status(200).send(req.body)
+})
+
 app.listen(process.env.PORT || 3000)
